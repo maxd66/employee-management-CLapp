5bb73c3649796d32ac02d2d4be922dd240bf05ce
maxd66 <max.dunn63@gmail.com>
2021-07-20 02:40:17 -0700

    added tests for validate integer functions

--- tests/Ask.test.js
+++ tests/Ask.test.js
@@ -1,3 +1,4 @@
+const { it, expect } = require('@jest/globals');
 const Ask = require('../lib/Ask');
 
 describe('Ask', () => {
@@ -11,6 +12,32 @@ describe('Ask', () => {
 
             expect(t).toBe(true);
             expect(f).toBe(false);
+        })
+    })
+    describe('validateInteger', () => {
+        it('Should return false for anything other number character', () => {
+            const ask = new Ask;
+            const num = 'Ellie';
+            const num2 = ']';
+            const ans = ask.validateInteger(num);
+            const ans2 = ask.validateInteger(num2);
+
+            expect(ans).toBe(false)
+            expect(ans2).toBe(false)
+        })
+        it('Should return false for any number that is not an integer', () => {
+            const ask = new Ask;
+            const num = 12.2;
+            const ans = ask.validateInteger(num);
+
+            expect(ans).toBe(false)
+        })
+        it('Should return true for integers', () => {
+            const ask = new Ask;
+            const num = 12;
+            const ans = ask.validateInteger(num);
+
+            expect(ans).toBe(true)
         })
     })
 })
